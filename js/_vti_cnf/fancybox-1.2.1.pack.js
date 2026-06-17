bf28217e14c37e704391f04f06c906ee4bd36ef6
gclcom <globalcarelogistics.com@gmail.com>
2026-06-17 16:36:43 +0530

    Add files via upload

---
+++ js/_vti_cnf/fancybox-1.2.1.pack.js
@@ -0,0 +1,7 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|21 Dec 2010 06:08:27 -0000
+vti_extenderversion:SR|5.0.2.6790
+vti_lineageid:SR|{29C3F042-7C4E-4D78-B4FC-2C8E06B85DBF}
+vti_cacheddtm:TX|21 Dec 2010 06:08:27 -0000
+vti_filesize:IR|8303
+vti_backlinkinfo:VX|
---
+++ js/_vti_cnf/jquery.easing.js
@@ -0,0 +1,7 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|21 Dec 2010 06:08:21 -0000
+vti_extenderversion:SR|5.0.2.6790
+vti_lineageid:SR|{7B802DE9-5EBD-4EBD-9D87-41D7580A83AC}
+vti_cacheddtm:TX|21 Dec 2010 06:08:21 -0000
+vti_filesize:IR|5099
+vti_backlinkinfo:VX|
